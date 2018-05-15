6f5058de5ea7d763bde64ce53f8c795205007cfa
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-05-15 00:12:24 -0400

    Test setup for postgres/mysql

--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -1,4 +1,10 @@
 ActiveRecord::Schema.define(:version => 0) do
+  adapter_type = connection.adapter_name.downcase.to_sym
+
+  if adapter_type == :postgresql
+    enable_extension "citext"
+    enable_extension "hstore"
+  end
 
   create_table :posts, :force => true do |t|
     t.string :first_name
@@ -6,19 +12,15 @@ ActiveRecord::Schema.define(:version => 0) do
     t.string :title
     t.text :summary
     t.text :body
-    t.column :slug, :citext
     t.integer :views
     t.integer :category_id
     t.string :blog_id
-    adapter_type = connection.adapter_name.downcase.to_sym
-    case adapter_type
-    when :postgresql
-      t.string :tags, array: true
-      t.hstore :custom_data
-    else
-      t.string :tags
-      t.integer :custom_data
+    t.json :json if %i[mysql2 postgresql].include? adapter_type
+    if adapter_type == :postgresql
+      t.citext :slug
+      t.string :array, array: true
+      t.hstore :hstore
+      t.jsonb :jsonb
     end
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,13 @@ require "yaml"
 # Establish DB Connection
 config = YAML::load(IO.read(File.join(File.dirname(__FILE__), 'db', 'database.yml')))
 ActiveRecord::Base.configurations = {'test' => config[ENV['DB'] || 'sqlite3']}
-ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations['test'])
+config = ActiveRecord::Base.configurations['test']
+if %w[mysql postgresql].include?(ENV['DB'])
+  ActiveRecord::Base.establish_connection(config.merge('database' => nil)) if ENV['DB'] == 'mysql'
+  ActiveRecord::Base.establish_connection(config.merge('database' => 'postgres')) if ENV['DB'] == 'postgresql'
+  ActiveRecord::Base.connection.recreate_database(config['database'], config)
+end
+ActiveRecord::Base.establish_connection(config)
 
 # Load Test Schema into the Database
 load(File.dirname(__FILE__) + "/db/schema.rb")
